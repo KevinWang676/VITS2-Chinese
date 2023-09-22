82b75629ef4356e9e518f07b654049e3a900e89e
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2023-09-22 00:35:48 -0700

    Create split.py

---
+++ split.py
@@ -0,0 +1,51 @@
+import subprocess
+import random
+import os
+from pathlib import Path
+import librosa
+from scipy.io import wavfile
+import numpy as np
+import torch
+import csv
+import whisper
+import argparse
+
+a="character"
+
+def split_long_audio(model, filepaths, save_dir="data_dir", out_sr=22050):
+    if isinstance(filepaths, str):
+        filepaths = [filepaths]
+
+    for file_idx, filepath in enumerate(filepaths):
+
+        save_path = Path(save_dir)
+        save_path.mkdir(exist_ok=True, parents=True)
+
+        print(f"Transcribing file {file_idx}: '{filepath}' to segments...")
+        result = model.transcribe(filepath, word_timestamps=True, task="transcribe", beam_size=5, best_of=5)
+        segments = result['segments']
+
+        wav, sr = librosa.load(filepath, sr=None, offset=0, duration=None, mono=True)
+        wav, _ = librosa.effects.trim(wav, top_db=20)
+        peak = np.abs(wav).max()
+        if peak > 1.0:
+            wav = 0.98 * wav / peak
+        wav2 = librosa.resample(wav, orig_sr=sr, target_sr=out_sr)
+        wav2 /= max(wav2.max(), -wav2.min())
+
+        for i, seg in enumerate(segments):
+            start_time = seg['start']
+            end_time = seg['end']
+            wav_seg = wav2[int(start_time * out_sr):int(end_time * out_sr)]
+            wav_seg_name = f"{a}_{i}.wav"
+            out_fpath = save_path / wav_seg_name
+            wavfile.write(out_fpath, rate=out_sr, data=(wav_seg * np.iinfo(np.int16).max).astype(np.int16))
+
+whisper_size = "large"
+whisper_model = whisper.load_model(whisper_size)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", default="linghua.wav")
+    args = parser.parse_args()
+    split_long_audio(whisper_model, f"{args.filename}", "./custom_character_voice/character/")
